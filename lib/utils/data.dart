19d4d3f360192e7adeff0d430f8708bc84ef1d07
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2022-04-09 22:02:34 +0700

    Project

---
+++ lib/utils/data.dart
@@ -0,0 +1,19 @@
+import 'package:project_recipes/utils/class.dart';
+
+class Data {
+  static List<Type> type = [
+    Type(
+      id: '1',
+      title: 'สูตรอาหารทั้งหมด',
+      imageUrl:
+      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    ),
+    Type(
+      id: '2',
+      title: 'สูตรอาหารยอดนิยม',
+      imageUrl:
+      'https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+
+    ),
+  ];
+}
